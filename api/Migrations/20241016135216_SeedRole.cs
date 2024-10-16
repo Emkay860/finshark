e43a8e29e3e0e450f952207ba024dbfc52c14305
Majid Kareem <43646844+Emkay860@users.noreply.github.com>
2024-10-16 15:01:28 +0100

    Add Account Controller and register endpoint

---
+++ api/Migrations/20241016135216_SeedRole.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "5eda878a-f6e2-4bc9-ae02-738747190cad", null, "User", "USER" },
+                    { "bdfb83ec-f879-4719-96c8-ca675fd8a853", null, "Admin", "ADMIN" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "5eda878a-f6e2-4bc9-ae02-738747190cad");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "bdfb83ec-f879-4719-96c8-ca675fd8a853");
+        }
+    }
+}
